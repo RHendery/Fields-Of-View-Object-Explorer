da46021af03e5279f2d87bf41cca89bcfa3c6208
andrew <andrew@miscellanea.com>
2019-05-08 17:07:52 +1000

    test monkey with internal handle

---
+++ Assets/2.scripts/hoverDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverDetection : MonoBehaviour {
+
+    Material material;
+
+    // Use this for initialization
+    void Start () {
+        material = GetComponent<Renderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void doit()
+    {
+        print("over");
+        material.color = new Color(1, 1, 1, 0.5f);
+    }
+
+    public void undoit()
+    {
+        material.color = new Color(1, 1, 1, 1);
+    }
+}
