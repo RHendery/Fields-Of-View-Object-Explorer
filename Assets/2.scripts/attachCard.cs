ca237c51dacb1327bd11094494bd9c695c1c8287
andrew <andrew@miscellanea.com>
2019-05-17 15:18:13 +1000

    move into zone and card pops up

---
+++ Assets/2.scripts/attachCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attachCard : MonoBehaviour
+{
+    public GameObject attachmentPoint;
+    public GameObject indexCard;
+    GameObject theInstatiatedIndexCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      if(attachmentPoint == null)
+        {
+            print("you need to drag an attachment point in the inspector");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void attachTheCard()
+    {
+        theInstatiatedIndexCard = Instantiate(indexCard, attachmentPoint.transform.position, attachmentPoint.transform.rotation);
+        theInstatiatedIndexCard.transform.parent = attachmentPoint.transform;
+    }
+
+    public void detachTheCard()
+    {
+        Destroy(theInstatiatedIndexCard);
+    }
+}
---
+++ Assets/2.scripts/overlapShowCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overlapShowCard : MonoBehaviour
+{
+    float sphereRadius;
+
+    void Start()
+    {
+        sphereRadius = transform.localScale.x / 2;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "museumObject")
+        {
+            print(other.name);
+            other.gameObject.SendMessage("attachTheCard");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "museumObject")
+        {
+            print(other.name);
+            other.gameObject.SendMessage("detachTheCard");
+        }
+    }
+
+
+}
