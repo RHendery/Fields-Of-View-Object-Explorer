fb28d3f784a0187df5a1b596dc131a737a87f8fb
RHendery <hendery@gmail.com>
2019-05-22 16:38:59 +1000

    created audio experiment scene, also grow/shrink kind of works

--- Assets/2.scripts/resizeObject.cs
+++ Assets/2.scripts/resizeObject.cs
@@ -5,7 +5,7 @@ using Leap.Unity.Interaction;
 
 public class resizeObject : MonoBehaviour
 {
-    float incrementSize = 0.001f; //increment to increase/decrease by. Change to speed up or slow down
+    float incrementSize = 0.01f; //increment to increase/decrease by. Change to speed up or slow down
     Controller controller;
     float previousPosition =0;
     float currentPosition = 0;
@@ -28,7 +28,7 @@ public class resizeObject : MonoBehaviour
     public void EnlargeObject() //this will be called once per frame because it's running on the "graspstay" interaction
     {
         Frame frame = controller.Frame(); // controller is a Controller object
-/*
+
         if (frame.Hands.Count > 0)
         {
             List<Hand> hands = frame.Hands;
@@ -41,32 +41,28 @@ public class resizeObject : MonoBehaviour
             var numberOfHands = gameObject.GetComponent<InteractionBehaviour>().graspingHands.Count;
             Debug.Log(numberOfHands);
 
-            while ( (numberOfHands > 1) ); //while hands are moving apart
+            if ( (numberOfHands > 1) ); //while hands are moving apart
             {
-                while (previousPosition < currentPosition)
+                if (previousPosition < currentPosition)
                 { 
                     Debug.Log("entered grow loop");
                     Debug.Log(gameObject.GetComponent<InteractionBehaviour>().graspingHands.Count);
                     gameObject.transform.localScale += new Vector3(incrementSize, incrementSize, incrementSize);
                     //       Debug.Log(string.Concat("Previously: " , previousPosition, "; Currently: ", currentPosition));
-                    previousPosition = currentPosition;
-                    currentPosition = secondHand.PalmPosition.x - firstHand.PalmPosition.x; //to be honest, I don't think it can update more than once a frame, so not sure if this will work
                 }
             
                 Debug.Log("exited grow loop");
 
-                while(previousPosition > currentPosition) //while hands are moving closer
+                if(previousPosition > currentPosition) //while hands are moving closer
                 {
                     Debug.Log("entered shrink loop");
                     gameObject.transform.localScale -= new Vector3(incrementSize, incrementSize, incrementSize); 
 
-                    previousPosition = currentPosition;
-                    currentPosition = secondHand.PalmPosition.x - firstHand.PalmPosition.x; //to be honest, I don't think it can update more than once a frame, so not sure if this will work
                 }
                 Debug.Log("exited shrink loop");
             }
         } 
-        */
+        
     }
 
     public void ShrinkObject()
