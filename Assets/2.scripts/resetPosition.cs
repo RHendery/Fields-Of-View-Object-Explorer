8898e73a821d31ef76394c2f92c4100129953ac2
andrew <andrew@miscellanea.com>
2019-05-24 13:35:31 +1000

    working on  sphere overlap and object knowledge of itself logic

--- Assets/2.scripts/resetPosition.cs
+++ Assets/2.scripts/resetPosition.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class resetPosition : MonoBehaviour
 {
     Vector3 startPos;
+    Quaternion startRot;
+    Rigidbody theAttachedRigidbody;
     
 
     // Start is called before the first frame update
@@ -12,7 +14,8 @@ public class resetPosition : MonoBehaviour
     {
 
         startPos = transform.position;
-   
+        startRot = transform.rotation;
+        theAttachedRigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,7 +24,10 @@ public class resetPosition : MonoBehaviour
     
         if (transform.position.y < -1)
         {
+            theAttachedRigidbody.isKinematic = true;
             transform.position = startPos;
+            transform.rotation = startRot;
+            theAttachedRigidbody.isKinematic = false;
         };
 
       
---
+++ Assets/2.scripts/soundOnOverlap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundOnOverlap : MonoBehaviour
+{
+    AudioSource theAudioSource;
+    public AudioClip theTouchSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playSound()
+    {
+        theAudioSource.clip = theTouchSound;
+        theAudioSource.Play();
+    }
+
+    public void stopPlayingSound()
+    {
+        theAudioSource.clip = theTouchSound;
+        theAudioSource.Stop();
+    }
+}
